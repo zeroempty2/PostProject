271b9412787b762ddb1a5ea6ed57896133516aa2
leeyoungbin <2zeroempty@gmail.com>
2023-01-02 15:36:04 +0900

    "<fix> UserDails에서 username을 받아올수 있도록 변경"

--- src/main/java/com/hannunpalzi/postproject/controller/LikeController.java
+++ src/main/java/com/hannunpalzi/postproject/controller/LikeController.java
@@ -2,35 +2,29 @@ package com.hannunpalzi.postproject.controller;
 
 import com.hannunpalzi.postproject.dto.LikeRequestDto;
 import com.hannunpalzi.postproject.dto.StatusResponseDto;
-import com.hannunpalzi.postproject.jwtUtil.JwtUtil;
+import com.hannunpalzi.postproject.security.UserDetailsImpl;
 import com.hannunpalzi.postproject.service.LikeService;
-import com.hannunpalzi.postproject.service.TokenChecker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
 public class LikeController {
     private final LikeService likeService;
-    private final JwtUtil jwtUtil;
-    private final TokenChecker tokenChecker;
-
     @PostMapping("/post/{id}/like")
-    public ResponseEntity<StatusResponseDto> postLike(@PathVariable Long id, @RequestBody LikeRequestDto likeRequestDto, HttpServletRequest request){
-        String token = jwtUtil.resolveToken(request);
-        String username = tokenChecker.checkToken(token);
+    public ResponseEntity<StatusResponseDto> postLike(@PathVariable Long id, @RequestBody LikeRequestDto likeRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        String username = userDetails.getUsername();
         return likeService.postLike(username,id,likeRequestDto);
     }
     @PostMapping("/comments/{commentId}/like")
-    public ResponseEntity<StatusResponseDto> commentLike(@PathVariable Long commentId, @RequestBody LikeRequestDto likeRequestDto, HttpServletRequest request){
-        String token = jwtUtil.resolveToken(request);
-        String username = tokenChecker.checkToken(token);
+    public ResponseEntity<StatusResponseDto> commentLike(@PathVariable Long commentId, @RequestBody LikeRequestDto likeRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        String username = userDetails.getUsername();
         return likeService.commentLike(username,commentId,likeRequestDto);
     }
 }
