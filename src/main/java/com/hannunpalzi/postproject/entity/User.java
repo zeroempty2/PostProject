c2d14dfc026508038c14748d235018f157b49984
DRIPMASTER <qsccsq4444@naver.com>
2023-01-03 19:04:10 +0900

    <feat>유저 삭제기능 (미완)

--- src/main/java/com/hannunpalzi/postproject/entity/User.java
+++ src/main/java/com/hannunpalzi/postproject/entity/User.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -22,6 +24,11 @@ public class User {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Comment> commentList = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Post> postList = new ArrayList<>();
 
     public User(String username, String password, UserRoleEnum role) {
         this.username = username;
